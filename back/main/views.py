808263e55e3d27912a8ed28e7fdfe2e20c3f666e
MineEric64 <besteric40@gmail.com>
2025-05-05 19:20:16 +0900

    Update views.py

--- back/main/views.py
+++ back/main/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+context = {
+
+}
+
 def index(request):
-    return render(request,'main/index.html')
+    return render(request,'main/index.html', context)
+
+#region 메인 페이지
+def add_news_articles():
+    # 뉴스 기사를 불러오고 (크롤링) post 데이터 (context)에 저장
+    pass
+
+def on_test_click():
+    # 검사 버튼 클릭 시, 테스트 페이지로 리다이렉트
+    pass
+
+def on_news_click():
+    # 뉴스 제목 클릭 시, 해당 뉴스 웹페이지로 리다이렉트
+    pass
+#endregion
