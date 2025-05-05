dff54d6dbb726c013671d009ada8c4223dda91b3
seokhan0731 <wistin@naver.com>
2025-05-05 21:05:31 +0900

    pageidxctrl

--- back/main/views.py
+++ back/main/views.py
@@ -21,3 +21,10 @@ def on_news_click():
     # 뉴스 제목 클릭 시, 해당 뉴스 웹페이지로 리다이렉트
     pass
 #endregion
+
+
+#질문페이지 인덱스 관리
+def PageIdxCtrl(request,page_num):
+    #유효 인덱스 확인, 인덱스에 해당하는 질문 가져와 넘겨주기(기존 응답이 있다면 응답까지)
+    #응답값 저장 후, 이전/다음 url로 리다이렉트
+    pass
