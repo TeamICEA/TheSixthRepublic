7b93344a65522cd002ab6a35113ee8ba83e2ab8d
seokhan0731 <wistin@naver.com>
2025-05-19 23:18:41 +0900

    url 충돌해결

--- back/main/urls.py
+++ back/main/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:str_id>/',views.IndividualPoliticians,name='IndividualPoliticians'),
+    path('ranking/',views.politician_ranking,name='politician_ranking'),
 ]
