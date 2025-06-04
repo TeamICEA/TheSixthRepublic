54a9e4dc7edf19ac1bf53a49dbb5abfcc5060350
MineEric64 <besteric40@gmail.com>
2025-06-04 21:12:33 +0900

    Update admin.py

--- back/main/admin.py
+++ back/main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Question)
+admin.site.register(Response)
+admin.site.register(Party)
+admin.site.register(PartyStance)
+admin.site.register(Politician)
+admin.site.register(Stance)
+admin.site.register(UserReport)
+admin.site.register(PoliticianReport)
+admin.site.register(Tone)
+admin.site.register(Chat)
